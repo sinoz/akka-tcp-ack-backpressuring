8ba15d4f3514eab3b614036f551094d84b3afb22
Sino <ilyas.alfan.b@gmail.com>
2018-01-06 17:13:18 +0100

    Ack defering helper trait

--- src/main/scala/org/sinoz/akka/tcp/backpressure/package.scala
+++ src/main/scala/org/sinoz/akka/tcp/backpressure/package.scala
@@ -1,9 +1,21 @@
 package org.sinoz.akka.tcp
 
+import akka.actor.Actor
+
 /** @author Sino */
 package object backpressure {
   case class ReadThrottleConfig(lowWaterMark: Long, highWaterMark: Long)
   case class WriteThrottleConfig()
 
   case class InboundConfig(readConfig: ReadThrottleConfig, writeConfig: WriteThrottleConfig)
+
+  trait AckHelper { actor: Actor =>
+    final def deferAck(ack: Any)(operation: => Unit): Unit = {
+      try {
+        operation
+      } finally {
+        sender() ! ack
+      }
+    }
+  }
 }
